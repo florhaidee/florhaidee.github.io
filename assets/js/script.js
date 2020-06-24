65297ab790cb68d1d8de12ce3cf565e3246ed2c6
florhaidee <63246286+florhaidee@users.noreply.github.com>
2020-06-24 14:02:32 -0600

    change style for contactme section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+/*$('#pdf-iframe').load(function(){
+    document.getElementById('pdf-iframe').print();
+});*/
 $(document).ready(function(){
     $('.carousel').carousel();
+    $('.modal').modal();
+    
 });
