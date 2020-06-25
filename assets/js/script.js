fa9d0679f1e9df4aadc49535ac2668c8142e307b
florhaidee <63246286+florhaidee@users.noreply.github.com>
2020-06-24 23:36:06 -0600

    responsive footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-/*$('#pdf-iframe').load(function(){
-    document.getElementById('pdf-iframe').print();
-});*/
+var saveResume = function(event){
+    event.preventDefault();
+    console.log("testing")
+}
 $(document).ready(function(){
     $('.carousel').carousel();
     $('.modal').modal();
-    
 });
